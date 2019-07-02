fe23ed92059f9bb33238477e4654836aca60a482
Tien Nguyen <tien.ng1020@gmail.com>
2019-07-02 16:29:30 -0500

    update

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+var NodeGeocoder = require("node-geocoder");
+var axios = require("axios");
 var Spotify = require('node-spotify-api');
-var keys = require("./keys")
+var keys = require("./keys");
+
+// --------------Spotify-------------
 
 var spotify = new Spotify(keys.spotify);
 
@@ -19,9 +23,6 @@ function findSpotify() {
         if (err) {
             return console.log(err)
         }
-
-        //  console.log(JSON.stringify(data,null,2));
-
 
         if (response.tracks.items[0] === undefined) {
           
@@ -51,4 +52,84 @@ function findSpotify() {
     })
 
 }
+
+// ---------------Movies------------
+
+var movieInput=process.argv.slice(3).join(" ");
+var userCommand = process.argv[2];
+
+switch (userCommand) {
+    case "movie-this":
+        findMovie();
+        break;
+
+}
+
+function findMovie(){
+
+axios.get("http://www.omdbapi.com/?t=" + movieInput + "=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("Movie title: ", response.data.Title, "\n ",
+                "Year: ", response.data.Year, "\n ",
+                "Rating: ", response.data.imdbRating, "\n ",
+                "Rotten Tomatoes: ", response.data.Ratings[1].Value, "\n ",
+                "Country: ", response.data.Country, "\n ",
+                "Language: ", response.data.Language, "\n ",
+                "Plot: ", response.data.Plot, "\n ",
+                "Actors: ", response.data.Actors, "\n ",
+                );
+  })
+  .catch(function(error) {
+    if (error.response) {
+        
+        axios.get("http://www.omdbapi.com/?t=Mr.Nobody=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("Movie title: ", response.data.Title, "\n ",
+                "Year: ", response.data.Year, "\n ",
+                "Rating: ", response.data.imdbRating, "\n ",
+                "Rotten Tomatoes: ", response.data.Ratings[1].Value, "\n ",
+                "Country: ", response.data.Country, "\n ",
+                "Language: ", response.data.Language, "\n ",
+                "Plot: ", response.data.Plot, "\n ",
+                "Actors: ", response.data.Actors, "\n ",
+                );
+  })
+      
+
+    }else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+}
+
+// ------------Concerts-----------
+
+var artist=process.argv.slice(3).join(" ");
+var userCommand = process.argv[2];
+
+switch (userCommand) {
+    case "concert-this":
+        bandsInTown();
+        break;
+
+}
+
+function bandsInTown(){
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    console.log("Venue: ", response.data[i].venue.name, "\n ",
+                "Location: ", response.data.Year, "\n ",
+                "Event Date: ", response.data[i].datetime, "\n ",
+                
+                );
+  })
+}
+
+
+
+
+
 
